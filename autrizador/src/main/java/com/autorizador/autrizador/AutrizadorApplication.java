0ec5d34d5c4b671c4fdac4ffd1872da89e916f49
omesa198 <omesa1984@gmail.com>
2022-10-06 16:53:44 -0300

    first commit

---
+++ autrizador/src/main/java/com/autorizador/autrizador/AutrizadorApplication.java
@@ -0,0 +1,13 @@
+package com.autorizador.autrizador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutrizadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutrizadorApplication.class, args);
+	}
+
+}
---
+++ autrizador/src/test/java/com/autorizador/autrizador/AutrizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autorizador.autrizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutrizadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
